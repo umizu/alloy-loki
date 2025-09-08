eadbbd9e851efb23de97aa300c1575d447be355a
umizu <ninjaz.gg@outlook.com>
2025-09-08 11:23:11 -0400

    mTLS

--- generate-certs.sh
+++ generate-certs.sh
@@ -2,8 +2,8 @@
 set -e
 
 CERT_DIR="/certs"
-mkdir -p "$CERT_DIR"
 
+# gen root ca
 openssl req -x509 -newkey rsa:4096 -sha256 -days 7300 \
   -keyout "$CERT_DIR/example-root.key" \
   -out "$CERT_DIR/example-root.crt" \
@@ -13,30 +13,33 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 7300 \
   -addext "keyUsage = critical, cRLSign, keyCertSign" \
   -addext "subjectKeyIdentifier = hash"
 
+# gen proxy leaf cert
 openssl req -new -newkey rsa:2048 -nodes \
   -keyout "$CERT_DIR/proxy.key" \
   -out "$CERT_DIR/proxy.csr" \
   -subj "/CN=proxy" \
   -addext "subjectAltName = DNS:proxy"
-echo "subjectAltName=DNS:proxy" > "$CERT_DIR/proxy.ext"
 openssl x509 -req -in "$CERT_DIR/proxy.csr" \
   -CA "$CERT_DIR/example-root.crt" \
   -CAkey "$CERT_DIR/example-root.key" \
   -set_serial 01 \
   -out "$CERT_DIR/proxy.crt" \
   -days 365 -sha256 \
-  -extfile "$CERT_DIR/proxy.ext"
+  -extfile <(echo "subjectAltName=DNS:proxy")
 
+# gen alloy leaf cert
 openssl req -new -newkey rsa:2048 -nodes \
   -keyout "$CERT_DIR/alloy.key" \
   -out "$CERT_DIR/alloy.csr" \
   -subj "/CN=alloy" \
   -addext "subjectAltName = DNS:alloy"
-echo "subjectAltName=DNS:alloy" > "$CERT_DIR/alloy.ext"
 openssl x509 -req -in "$CERT_DIR/alloy.csr" \
   -CA "$CERT_DIR/example-root.crt" \
   -CAkey "$CERT_DIR/example-root.key" \
   -set_serial 02 \
   -out "$CERT_DIR/alloy.crt" \
   -days 365 -sha256 \
-  -extfile "$CERT_DIR/alloy.ext"
+  -extfile <(echo "subjectAltName=DNS:proxy")
+
+# cleanup
+rm $CERT_DIR/proxy.csr $CERT_DIR/alloy.csr
--- readme.md
+++ readme.md
@@ -1,25 +1,2 @@
 # grafana-alloy-loki
 
-before running compose file
-
-```bash
-# gen root CA
-openssl req -x509 -newkey rsa:4096 -sha256 -days 7300 \
-  -keyout certs/example-root.key -out certs/example-root.crt \
-  -noenc \
-  -subj "/CN=example-root" \
-  -addext "basicConstraints = CA:TRUE" \
-  -addext "keyUsage = critical, cRLSign, keyCertSign" \
-  -addext "subjectKeyIdentifier = hash"
-
-# gen CSR for proxy
-openssl req -new -newkey rsa:2048 -nodes \
-  -keyout certs/proxy.key -out certs/proxy.csr \
-  -subj "/CN=proxy" \
-  -addext "subjectAltName = DNS:proxy"
-
-# sign proxy cert with root CA
-openssl x509 -req -in certs/proxy.csr -CA certs/example-root.crt -CAkey certs/example-root.key \
-    -CAcreateserial -out certs/proxy.crt -days 365 -sha256 \
-    -extfile <(echo "subjectAltName=DNS:proxy")
-```
